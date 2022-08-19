8b68734d2e0fd103cad290b7e8994a55f5eecdea
milkyway90 <ignacymilocha4@gmail.com>
2022-08-19 16:11:06 +0200

    updated stuff for phones

--- graphics.js
+++ graphics.js
@@ -1,6 +1,6 @@
 const app = new PIXI.Application({
-    width: 800,
-    height: 600,
+    width: 80,
+    height: 60,
     backgroundColor: 0xbfbfbd
     
 });
@@ -14,8 +14,8 @@ canvas.transform = 'translate(-50%, -50%)'
 document.body.appendChild(app.view,);
 const dvd_logo = PIXI.Sprite.from('dvd_logo.png');
 
-dvd_logo._height = 100;
-dvd_logo._width = 200;
+dvd_logo._height = 10;
+dvd_logo._width = 20;
 
 app.stage.addChild(dvd_logo);
 
@@ -26,13 +26,13 @@ app.ticker.add(function() {
     dvd_logo.x += value_x;
     dvd_logo.y += value_y; 
 
-    if(dvd_logo.y == (app.view.height - 100)){
+    if(dvd_logo.y == (app.view.height - 10)){
         value_y = -1;
     } else if(dvd_logo.y == 0){
         value_y = 1;
     } 
 
-    if(dvd_logo.x == (app.view.width - 200)){
+    if(dvd_logo.x == (app.view.width - 20)){
         value_x = -1;
     } else if(dvd_logo.x == 0) {
         value_x = 1;
